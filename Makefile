161f46156c2e3773923ffd766bb2d96a3aa9be99
nyaxt <ueno@nyaxtstep.com>
2012-10-21 10:46:55 +0900

    trying out leg

--- Makefile
+++ Makefile
@@ -1,2 +1,11 @@
-parse.impl.h: lang.rl
-	ragel -C $< -o $@
+CXXFLAGS=--std=c++0x -O0 -g
+
+test: ncc
+	echo "1 + 2 + 3" | ncc
+
+ncc: main.cpp parse.impl.h
+	g++ $(CXXFLAGS) -o $@ $<
+
+parse.impl.h: lang.leg
+	leg -o $@ $<
+
--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <memory>
+#include <string.h>
 
 class Parser
 {
@@ -15,13 +17,36 @@ private:
 	std::string m_code;
 };
 
+#define YY_CTX_LOCAL
 #include "parse.impl.h"
+
+namespace {
+
+void yyfree(yycontext* ctx)
+{
+	if(ctx->buf) ::free(ctx->buf);
+	if(ctx->text) ::free(ctx->text);
+	if(ctx->thunks) ::free(ctx->thunks);
+	if(ctx->vals) ::free(ctx->vals);
+}
+
+} // end of anonymous namespace
+
+void
+Parser::parse()
+{
+	yycontext ctx;
+	::memset(&ctx, 0, sizeof(yycontext));
+
+	printf("yyparse(): %d\n", yyparse(&ctx));
+
+	yyfree(&ctx);
+}
 
 int
 main(int argc, char* argv[])
 {
-	Parser p(argv[1]);
-	p.parse();
+	Parser p("1+2"); p.parse();
 	
 	return 0;
 }
