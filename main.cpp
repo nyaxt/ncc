4993f9c36e6acc67abf9247d7ce4d077214954e2
nyaxt <ueno@nyaxtstep.com>
2012-10-21 17:56:40 +0900

    can parse: 1 - 2 + 3

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <functional>
+#include <algorithm>
 #include <string.h>
 
 //! ast node interface
@@ -28,13 +30,13 @@ ASTNode::~ASTNode()
 }
 
 //! liretal
-class ValL : public ASTNode
+class ConstNode : public ASTNode
 {
 public:
-	ValL(int n)
+	ConstNode(int n)
 	: m_n(n) { /* NOP */ }
 
-	~ValL();
+	~ConstNode();
 
 	virtual void dump(std::ostream& s) const override;	
 
@@ -42,15 +44,47 @@ private:
 	int m_n;
 };
 
-ValL::~ValL()
+ConstNode::~ConstNode()
 {
 
 }
 
 void
-ValL::dump(std::ostream& s) const
+ConstNode::dump(std::ostream& s) const
 {
-	s << "<ValL: " << m_n << ">";
+	s << "<ConstNode: " << m_n << ">";
+}
+
+class ApplyNode : public ASTNode
+{
+public:
+	ApplyNode(std::string funcname, std::vector<ASTNode*> args)
+	:	m_funcname(std::move(funcname)), m_args(std::move(args))
+	{ /* NOP */ }
+
+	~ApplyNode();
+
+	virtual void dump(std::ostream& s) const override;	
+
+private:
+	std::string m_funcname;
+	std::vector<ASTNode*> m_args;
+};
+
+ApplyNode::~ApplyNode()
+{
+	/* NOP */
+}
+
+void
+ApplyNode::dump(std::ostream& s) const
+{
+	s << "<ApplyNode: " << m_funcname << "(";
+	for(ASTNode* arg: m_args)
+	{
+		s << *arg << ", ";
+	}
+	s << ")>";
 }
 
 class Parser
@@ -71,6 +105,8 @@ public:
 
 	noded_t addNode(ASTNode* newnode)
 	{
+		std::cout << "addNode: " << *newnode << std::endl;
+
 		m_astnodes.push_back(PASTNode(newnode));
 		return m_astnodes.size() - 1;
 	}
@@ -78,6 +114,14 @@ public:
 	ASTNode* getNode(noded_t s)
 	{
 		return m_astnodes[s].get();	
+	}
+
+	std::vector<ASTNode*> getNodes(const std::vector<noded_t>& nodeds)
+	{
+		std::vector<ASTNode*> ret; ret.resize(nodeds.size());
+		std::transform(nodeds.begin(), nodeds.end(), ret.begin(), std::bind(&Parser::getNode, this, std::placeholders::_1));
+
+		return std::move(ret);
 	}
 
 	void setRootNode(noded_t s)
@@ -137,6 +181,7 @@ main(int argc, char* argv[])
 {
 	Parser p("1+2"); p.parse();
 	p.dump(std::cout);
+	std::cout << std::endl;
 	
 	return 0;
 }
